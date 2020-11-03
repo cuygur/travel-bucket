d5376128d770d47fa0d854723a8a84f42c087a18
Cihan Uygur <54838188+cuygur@users.noreply.github.com>
2020-11-03 22:51:11 +0300

    fix eslint error

--- src/Components/Create.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
-import firebase from './Firebase';
-import db from "./firebaseConfig";
-
